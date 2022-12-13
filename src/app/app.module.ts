a7c12fc2e28f225b416369d46eb1231b4aabf8cc
athulya2000 <athulyav00@gmail.com>
2022-12-13 12:34:43 +0530

    adminlogin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { Routes } from '@angular/router';
+import { Navbar1Component } from './navbar1/navbar1.component';
+
+const myRoute:Routes=[
+  {
+    path:"",
+    component:AdminLoginComponent
+  }
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminLoginComponent,
+    Navbar1Component
   ],
   imports: [
     BrowserModule,
